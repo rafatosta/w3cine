2977c53ea53d2a0618f75f11a1c6b018a99a0f95
Rafael Tosta <rafa.ecomp@gmail.com>
2023-03-30 11:01:52 -0300

    :sparkles: feat: Rota para o dashboard do Adm

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+function Dashboard() {
+    return ( <div>Dashboard do Administrador</div> );
+}
+
+export default Dashboard;
--- app/movie/[pid]/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-function MovieID({ params }: { params: { pid: string } }) {
-  return <div>Filme ID = {params.pid}</div>;
-}
-
-export default MovieID;
